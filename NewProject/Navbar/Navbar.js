c817bb6911bb521843ff86584529c64dd916d17f
Kanishk-K <Kanishkkacholia@Gmail.com>
2020-08-19 23:08:26 -0500

    Change Class JQuery Navbar

--- NewProject/Navbar/Navbar.js
+++ NewProject/Navbar/Navbar.js
@@ -9,3 +9,15 @@ $(".btn-close").click(function(e){
     $(".navbar-collapse").removeClass("show");
     $("body").removeClass("offcanvas-active");
 }); 
+
+$(window).on("load resize scroll",function(){
+    var windowTop = $(window).scrollTop(); //Distance from top of the page, increases as user scrolls down.
+    var elementTop = $(".parent").offset().top; //Constant as to where an object's y position on the screen is.
+    var Bounds = windowTop - elementTop; //Negative at first but as the element goes above the user's screen it becomes positive
+    if (Bounds >= 0){
+        $(".navbar").addClass("bg-dark");
+    }
+    else{
+        $(".navbar").removeClass("bg-dark");
+    }
+});
