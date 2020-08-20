7779ad273ea10ef772b213cbb7800d8b0ffb2b9a
Kanishk-K <Kanishkkacholia@Gmail.com>
2020-08-20 13:21:04 -0500

    Added Stats bar

--- NewProject/Navbar/Navbar.js
+++ NewProject/Navbar/Navbar.js
@@ -12,7 +12,7 @@ $(".btn-close").click(function(e){
 
 $(window).on("load resize scroll",function(){
     var windowTop = $(window).scrollTop(); //Distance from top of the page, increases as user scrolls down.
-    var elementTop = $(".parent").offset().top; //Constant as to where an object's y position on the screen is.
+    var elementTop = $(".stat-holder").offset().top; //Constant as to where an object's y position on the screen is.
     var Bounds = windowTop - elementTop; //Negative at first but as the element goes above the user's screen it becomes positive
     if (Bounds >= 0){
         $(".navbar").css({"background-color":"#292b2c"});
